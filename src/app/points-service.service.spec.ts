e7c2de79a5f243e93a1b3238612d606aadd6c298
Zygimantas <benetis.zygimantas@gmail.com>
2017-05-11 21:02:28 +0300

    feat(points-service) add, get points

--- src/app/points-service.service.spec.ts
+++ src/app/points-service.service.spec.ts
@@ -1,16 +1,53 @@
-import { TestBed, inject } from '@angular/core/testing';
+import {inject, TestBed} from '@angular/core/testing';
 
-import { PointsServiceService } from './points-service.service';
-import {of} from 'rxjs/observable/of';
+import {PointsServiceService} from './points-service.service';
 
 fdescribe('PointsServiceService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [PointsServiceService]
-    });
-  });
+    let service;
 
-  it('should return observable with previously set points', inject([PointsServiceService], (service: PointsServiceService) => {
-  }));
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [PointsServiceService]
+        });
+    });
+
+    beforeEach(inject([PointsServiceService], (srv: PointsServiceService) => {
+        service = srv;
+    }));
+
+    it('should return empty observable when called getPoints', done => {
+        service.getPoints().subscribe(p => {
+            expect(p).toEqual([]);
+            done();
+        })
+    })
+
+    it('should add points and return all points', done => {
+        const pointsToAdd = [
+            {x: 1, y: 1},
+            {x: 0, y: 1},
+        ]
+
+        service.addPoints(pointsToAdd).subscribe(p => {
+            expect(p).toEqual(pointsToAdd);
+            done();
+        })
+    })
+
+    it('should add points twice and return all points', done => {
+        const pointsToAdd = [
+            {x: 1, y: 1},
+            {x: 0, y: 1},
+        ]
+
+        service.addPoints(pointsToAdd)
+        service.addPoints(pointsToAdd)
+
+        service.getPoints().subscribe(p => {
+            expect(p).toEqual([...pointsToAdd, ...pointsToAdd]);
+            done();
+        })
+    })
+
 
 });
--- src/app/points-service.service.ts
+++ src/app/points-service.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Point} from './points-table/points-table.component';
+import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class PointsServiceService {
 
-  constructor() { }
+    private points: Point[] = [];
+
+    constructor() {
+    }
+
+    public getPoints(): Observable<Point[]> {
+        return new BehaviorSubject(this.points);
+    }
+
+    public addPoints(pointsToAdd): Observable<Point[]> {
+        this.points = this.points.concat(pointsToAdd)
+        return this.getPoints()
+    }
 
 }
